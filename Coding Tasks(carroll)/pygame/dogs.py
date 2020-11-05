2aecd40b2d37c0f61ccf050e249ec6aa54d771f7
lawman123 <ytubelawman123@gmail.com>
2020-11-05 13:57:36 +0000

    Update dogs.py

--- Coding Tasks(carroll)/pygame/dogs.py
+++ Coding Tasks(carroll)/pygame/dogs.py
@@ -7,8 +7,23 @@ class Dog():
     def get_colour():
         return self.colour
 
+class Puppy(Dog):
+    def __init__ (self, name, colour):
+        super().__init__(name, colour)
+        self.shoesChewed = 0
+    def chewShoe(self, numShoes):
+        self.shoesChewed += numShoes
+    def getShoes(self):
+        return self.shoesChewed
+
 my_dog1 = Dog("Fido","Brown")
 my_dog2 = Dog("Rex","Black")
+
+my_pappy1 = Puppy("Dave","Purple")
+
+my_pappy1.chewShoe(3)
+
+print(my_pappy1.name,my_pappy1.colour,my_pappy1.getShoes())
 
 print(my_dog1.name,my_dog1.colour)
 print(my_dog2.name,my_dog2.colour)
